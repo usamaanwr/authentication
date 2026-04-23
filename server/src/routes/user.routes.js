03525d73996679c8d3fd527e1829f03f9769782d
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-04-24 01:58:11 +0500

    Add login and logout routes with JWT verification to user router

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { registerUser } from "../controller/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controller/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js"; 
+import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = Router()
 router.post('/register', upload.fields([{
     name: "avatar",
     maxCount: 1
 }]), registerUser)
 
+router.post("/login" , loginUser)
+router.post("/logout" , verifyJWT, logoutUser)
 export const userRouter = router
 
