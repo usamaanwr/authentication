473e059223e444919f5ac118fc2901ac743ef266
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-04-29 01:20:49 +0500

    fix: switch multer storage to memoryStorage for improved performance

--- server/src/middleware/multer.middleware.js
+++ server/src/middleware/multer.middleware.js
@@ -1,12 +1,16 @@
 import multer from "multer";
-import path from "path"
-const storage = multer.diskStorage({
-    destination: function(req , file , cb){
-        cb(null, path.join(process.cwd(), "public/temp"))
-    },
-    filename: function(req , file , cb){
-        cb(null , file.originalname)
-    }
-})
 
-export const upload = multer({storage: storage})
+const storage = multer.memoryStorage();
+
+export const upload = multer({ storage });
+// import path from "path"
+// const storage = multer.memoryStorage({
+//     destination: function(req , file , cb){
+//         cb(null, path.join(process.cwd(), "public/temp"))
+//     },
+//     filename: function(req , file , cb){
+//         cb(null , file.originalname)
+//     }
+// })
+
+// export const upload = multer({storage: multer.memoryStorage()})
