13d3624618f3c8a196c46441221e10e133698ff4
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-04-29 01:43:14 +0500

    fix: correct MongoDB URI environment variable name

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 const connectDb = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_BD_URI}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MMONGODB_URI}`)
         console.log("MONGO DB IS CONNECTED !!!");
         
     } catch (error) {
