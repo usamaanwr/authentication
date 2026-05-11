8039790eeb668e06aa37f6f7bd486349a513947b
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-05-11 14:53:47 +0500

    fix: simplify database connection handling in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,8 @@ import app from "./app.js";
 import dotenv from "dotenv"
 dotenv.config()
 import connectDb from "./db/index.js";
-const port = process.env.PORT
+
+
 connectDb()
 
-.then(()=>{
-app.listen(port, ()=>{
-    console.log(`server is start in running:${port}`);
-})
-})
-.catch((err)=>{
-    console.log("mongo db connection is failed");
-})
-
+export default app  
