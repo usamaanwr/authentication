8c1aa966fbbe7b0377c2a33447baf5db319d926d
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-05-13 01:42:58 +0500

    feat(client): set up initial React application structure with routing and styling

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <AuthProvider>
+        <Navbar />
+
+        {/* Saare pages ke routes yahan handle honge */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={ <ProtectedRoute>
+            < Dashboard />
+          </ProtectedRoute>} />
+        </Routes>
+
+      </AuthProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+
+export default App
